6f877176696789f868131f8265a4b679a907bf32
Daniele Grattarola <daniele.grattarola@gmail.com>
2019-11-15 15:32:11 -0500

    Add citation and ppi benchmarks Simplify ARMAConv layer

---
+++ tests/benchmarks/citation/main.sh
@@ -0,0 +1,14 @@
+python3 citation.py --dataset 'cora' --neighbourhood 1
+python3 citation.py --dataset 'cora' --neighbourhood 2
+python3 citation.py --dataset 'cora' --neighbourhood 3
+python3 citation.py --dataset 'cora' --neighbourhood 4
+
+python3 citation.py --dataset 'citeseer' --neighbourhood 1
+python3 citation.py --dataset 'citeseer' --neighbourhood 2
+python3 citation.py --dataset 'citeseer' --neighbourhood 3
+python3 citation.py --dataset 'citeseer' --neighbourhood 4
+
+python3 citation.py --dataset 'pubmed' --neighbourhood 1
+python3 citation.py --dataset 'pubmed' --neighbourhood 2
+python3 citation.py --dataset 'pubmed' --neighbourhood 3
+python3 citation.py --dataset 'pubmed' --neighbourhood 4
