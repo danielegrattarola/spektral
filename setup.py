fdfcc9a2a71728eea10caeb76609c47e51087f8c
Daniele Grattarola <daniele.grattarola@gmail.com>
2019-11-14 19:16:57 -0500

    v0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='spektral',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     install_requires=['keras<2.3',
                       'tensorflow<2.0.0',
--- spektral/__init__.py
+++ spektral/__init__.py
@@ -3,4 +3,4 @@ from . import datasets
 from . import layers
 from . import utils
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
