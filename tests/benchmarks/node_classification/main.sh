c96c301adb1e94f89c0d93239101168cd775d6e1
Daniele Grattarola <daniele.grattarola@gmail.com>
2019-11-15 19:35:05 -0500

    Unify node classification benchmarks

---
+++ tests/benchmarks/node_classification/main.sh
@@ -0,0 +1,19 @@
+python3 node_classification.py --dataset 'cora' --neighbourhood 1
+python3 node_classification.py --dataset 'cora' --neighbourhood 2
+python3 node_classification.py --dataset 'cora' --neighbourhood 3
+python3 node_classification.py --dataset 'cora' --neighbourhood 4
+
+python3 node_classification.py --dataset 'citeseer' --neighbourhood 1
+python3 node_classification.py --dataset 'citeseer' --neighbourhood 2
+python3 node_classification.py --dataset 'citeseer' --neighbourhood 3
+python3 node_classification.py --dataset 'citeseer' --neighbourhood 4
+
+python3 node_classification.py --dataset 'pubmed' --neighbourhood 1
+python3 node_classification.py --dataset 'pubmed' --neighbourhood 2
+python3 node_classification.py --dataset 'pubmed' --neighbourhood 3
+python3 node_classification.py --dataset 'pubmed' --neighbourhood 4
+
+python3 node_classification.py --dataset 'ppi' --neighbourhood 1
+python3 node_classification.py --dataset 'ppi' --neighbourhood 2
+python3 node_classification.py --dataset 'ppi' --neighbourhood 3
+python3 node_classification.py --dataset 'ppi' --neighbourhood 4
