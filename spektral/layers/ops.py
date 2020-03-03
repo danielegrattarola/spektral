f7f0956aca1057aa15703cba93e470accf27de2d
Daniele Grattarola <daniele.grattarola@gmail.com>
2020-03-03 17:42:37 -0500

    Rewrite TUD loader to account for regression datasets Minor changes

--- spektral/layers/ops.py
+++ spektral/layers/ops.py
@@ -400,6 +400,7 @@ def segment_top_k(x, I, ratio, top_k_var):
     :return: a rank 1 Tensor containing the indices to get the top K values of
     each segment in x.
     """
+    I = tf.cast(I, tf.int32)
     num_nodes = tf.math.segment_sum(tf.ones_like(I), I)  # Number of nodes in each graph
     cumsum = tf.cumsum(num_nodes)  # Cumulative number of nodes (A, A+B, A+B+C)
     cumsum_start = cumsum - num_nodes  # Start index of each graph
@@ -407,7 +408,7 @@ def segment_top_k(x, I, ratio, top_k_var):
     max_n_nodes = tf.reduce_max(num_nodes)  # Order of biggest graph in batch
     batch_n_nodes = tf.shape(I)[0]  # Number of overall nodes in batch
     to_keep = tf.math.ceil(ratio * tf.cast(num_nodes, tf.float32))
-    to_keep = tf.cast(to_keep, tf.int32)  # Nodes to keep in each graph
+    to_keep = tf.cast(to_keep, I.dtype)  # Nodes to keep in each graph
 
     index = tf.range(batch_n_nodes)
     index = (index - tf.gather(cumsum_start, I)) + (I * max_n_nodes)
