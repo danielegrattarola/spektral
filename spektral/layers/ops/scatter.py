135ed41cff7c0e30d520b366b034b0f535322a0c
Daniele Grattarola <daniele.grattarola@gmail.com>
2020-03-28 20:18:08 +0100

    Undo incorrect commit for ops.scatter, ops.sparse, GIN and GS

--- spektral/layers/ops/scatter.py
+++ spektral/layers/ops/scatter.py
@@ -2,20 +2,20 @@ import tensorflow as tf
 
 
 def scatter_sum(indices, updates, N):
-    return tf.math.unsorted_segment_sum(indices, updates, N)
+    return tf.math.unsorted_segment_sum(updates, indices, N)
 
 
 def scatter_mean(indices, updates, N):
-    return tf.math.unsorted_segment_mean(indices, updates, N)
+    return tf.math.unsorted_segment_mean(updates, indices, N)
 
 
 def scatter_max(indices, updates, N):
-    return tf.math.unsorted_segment_max(indices, updates, N)
+    return tf.math.unsorted_segment_max(updates, indices, N)
 
 
 def scatter_min(indices, updates, N):
-    return tf.math.unsorted_segment_min(indices, updates, N)
+    return tf.math.unsorted_segment_min(updates, indices, N)
 
 
 def scatter_prod(indices, updates, N):
-    return tf.math.unsorted_segment_prod(indices, updates, N)
+    return tf.math.unsorted_segment_prod(updates, indices, N)
