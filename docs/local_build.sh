31808328eac7356208b752d712b2a1bbb8733989
Levi Borodenko <levi.borodenko@gmail.com>
2020-04-28 21:50:44 +0100

    ENH (issue #49): Added DiffusionConvolution, tests and a script to easily build docs locally.
    - Added DiffusionConvolution to `layers.convolutional`. Also covered it in tests and made sure the docs look good.
    - Improved docs for SortPool and covered it in globalpool tests (removed from pool tests).
    - Added both SortPool and DiffusionConvolution to `autogen.py` so it is contained in the docs.
    - added `docs/local_build.sh` which is a script that installs the current spektral build from the local branch, builds docs and serves them on a local webserver.
     For convinience.

---
+++ docs/local_build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# install current branch
+cd ../
+python setup.py install
+cd docs/
+
+# delete old docs
+rm -r sources/
+
+# generate new docs
+python autogen.py
+
+# serve new docs
+python -m mkdocs serve
--- tests/test_layers/test_global_pooling.py
+++ tests/test_layers/test_global_pooling.py
@@ -2,7 +2,7 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras import Input, Model
 
-from spektral.layers import GlobalSumPool, GlobalAttnSumPool, GlobalAttentionPool, GlobalAvgPool, GlobalMaxPool
+from spektral.layers import GlobalSumPool, GlobalAttnSumPool, GlobalAttentionPool, GlobalAvgPool, GlobalMaxPool, SortPool
 
 tf.keras.backend.set_floatx('float64')
 batch_size = 32
@@ -57,8 +57,32 @@ def _test_graph_mode(layer, **kwargs):
     output = model([X, S])
     assert output.shape == (batch_size, kwargs.get('channels', F))
     # When creating actual graph, the bacth dimension is None
-    assert output.shape[1:] == layer_instance.compute_output_shape([X.shape, S.shape])[1:]
+    assert output.shape[1:] == layer_instance.compute_output_shape([X.shape, S.shape])[
+        1:]
     _check_output_and_model_output_shapes(output.shape, model.output_shape)
+
+
+def _test_sortpool_single(layer, k):
+    X = np.random.normal(size=(N, F))
+    X_in = Input(shape=(F,))
+    layer_instance = layer(k=k)
+    output = layer_instance(X_in)
+    model = Model(X_in, output)
+
+    output = model(X)
+    assert output.shape == (k, F)
+    assert output.shape == layer_instance.compute_output_shape(X.shape)
+
+
+def _test_sortpool_batched(layer, k):
+    X = np.random.normal(size=(batch_size, N, F))
+    X_in = Input(shape=(N, F))
+    layer_instance = layer(k=k)
+    output = layer_instance(X_in)
+    model = Model(X_in, output)
+    output = model(X)
+    assert output.shape == (batch_size, k, F)
+    assert output.shape == layer_instance.compute_output_shape(X.shape)
 
 
 def test_global_sum_pool():
@@ -91,3 +115,8 @@ def test_global_attention_pool():
     _test_single_mode(GlobalAttentionPool, channels=F_)
     _test_batch_mode(GlobalAttentionPool, channels=F_)
     _test_graph_mode(GlobalAttentionPool, channels=F_)
+
+
+def test_global_sort_pool():
+    _test_sortpool_single(SortPool, k=6)
+    _test_sortpool_batched(SortPool, k=6)
