853d578c6c8b775779e656ba4b0c2fb2a09513dd
Dominic Jack <thedomjack@gmail.com>
2020-12-24 10:27:41 +1000

    Made GCN/GAT more consistent with original

--- examples/node_prediction/citation_gat.py
+++ examples/node_prediction/citation_gat.py
@@ -4,9 +4,11 @@ This example implements the experiments on citation networks from the paper:
 Graph Attention Networks (https://arxiv.org/abs/1710.10903)
 Petar Veličković, Guillem Cucurull, Arantxa Casanova, Adriana Romero, Pietro Liò, Yoshua Bengio
 """
-
+import numpy as np
+from tensorflow.random import set_seed
 from tensorflow.keras.callbacks import EarlyStopping
 from tensorflow.keras.layers import Input, Dropout
+from tensorflow.keras.losses import CategoricalCrossentropy
 from tensorflow.keras.models import Model
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.regularizers import l2
@@ -16,20 +18,27 @@ from spektral.datasets.citation import Citation
 from spektral.layers import GATConv
 from spektral.transforms import LayerPreprocess, AdjToSpTensor
 
+set_seed(0)
+
 # Load data
 dataset = Citation('cora',
+                   normalize_x=True,
                    transforms=[LayerPreprocess(GATConv), AdjToSpTensor()])
-mask_tr, mask_va, mask_te = dataset.mask_tr, dataset.mask_va, dataset.mask_te
+
+def mask_to_weights(mask):
+    return mask.astype(np.float32) / np.count_nonzero(mask)
+
+weights_tr, weights_va, weights_te = (mask_to_weights(mask) for mask in (
+      dataset.mask_tr, dataset.mask_va, dataset.mask_te))
 
 # Parameters
 channels = 8           # Number of channels in each head of the first GAT layer
 n_attn_heads = 8       # Number of attention heads in first GAT layer
 dropout = 0.6          # Dropout rate for the features and adjacency matrix
-l2_reg = 5e-6          # L2 regularization rate
+l2_reg = 2.5e-4        # L2 regularization rate
 learning_rate = 5e-3   # Learning rate
 epochs = 20000         # Number of training epochs
 patience = 100         # Patience for early stopping
-a_dtype = dataset[0].a.dtype  # Only needed for TF 2.1
 
 N = dataset.n_nodes          # Number of nodes in the graph
 F = dataset.n_node_features  # Original size of node features
@@ -37,7 +46,7 @@ n_out = dataset.n_labels     # Number of classes
 
 # Model definition
 x_in = Input(shape=(F,))
-a_in = Input((N,), sparse=True, dtype=a_dtype)
+a_in = Input((N,), sparse=True)
 
 do_1 = Dropout(dropout)(x_in)
 gc_1 = GATConv(channels,
@@ -46,7 +55,8 @@ gc_1 = GATConv(channels,
                dropout_rate=dropout,
                activation='elu',
                kernel_regularizer=l2(l2_reg),
-               attn_kernel_regularizer=l2(l2_reg)
+               attn_kernel_regularizer=l2(l2_reg),
+               bias_regularizer=l2(l2_reg),
                )([do_1, a_in])
 do_2 = Dropout(dropout)(gc_1)
 gc_2 = GATConv(n_out,
@@ -55,20 +65,21 @@ gc_2 = GATConv(n_out,
                dropout_rate=dropout,
                activation='softmax',
                kernel_regularizer=l2(l2_reg),
-               attn_kernel_regularizer=l2(l2_reg)
+               attn_kernel_regularizer=l2(l2_reg),
+               bias_regularizer=l2(l2_reg),
                )([do_2, a_in])
 
 # Build model
 model = Model(inputs=[x_in, a_in], outputs=gc_2)
 optimizer = Adam(lr=learning_rate)
 model.compile(optimizer=optimizer,
-              loss='categorical_crossentropy',
+              loss=CategoricalCrossentropy(reduction='sum'),
               weighted_metrics=['acc'])
 model.summary()
 
 # Train model
-loader_tr = SingleLoader(dataset, sample_weights=mask_tr)
-loader_va = SingleLoader(dataset, sample_weights=mask_va)
+loader_tr = SingleLoader(dataset, sample_weights=weights_tr)
+loader_va = SingleLoader(dataset, sample_weights=weights_va)
 model.fit(loader_tr.load(),
           steps_per_epoch=loader_tr.steps_per_epoch,
           validation_data=loader_va.load(),
@@ -78,7 +89,7 @@ model.fit(loader_tr.load(),
 
 # Evaluate model
 print('Evaluating model.')
-loader_te = SingleLoader(dataset, sample_weights=mask_te)
+loader_te = SingleLoader(dataset, sample_weights=weights_te)
 eval_results = model.evaluate(loader_te.load(), steps=loader_te.steps_per_epoch)
 print('Done.\n'
       'Test loss: {}\n'
