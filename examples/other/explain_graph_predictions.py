7299a24ca51980881aad27b220aab53cd24c1ea1
Sasha Melnic <56506760+AlexandrMelnic@users.noreply.github.com>
2021-03-31 22:43:51 +0200

    used isort

--- examples/other/explain_graph_predictions.py
+++ examples/other/explain_graph_predictions.py
@@ -1,12 +1,13 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
-from spektral.data import DisjointLoader
-from spektral.datasets import TUDataset
-from spektral.models import GeneralGNN, GNNExplainer
 from tensorflow.keras.losses import CategoricalCrossentropy
 from tensorflow.keras.metrics import categorical_accuracy
 from tensorflow.keras.optimizers import Adam
+
+from spektral.data import DisjointLoader
+from spektral.datasets import TUDataset
+from spektral.models import GeneralGNN, GNNExplainer
 
 ######### Model setup ##################
 
