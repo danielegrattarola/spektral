6c77083b855b1647661bf64980400b668fb64fc8
Daniele Grattarola <daniele.grattarola@gmail.com>
2021-08-23 15:02:23 +0200

    v1.0.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="spektral",
-    version="1.0.7",
+    version="1.0.8",
     packages=find_packages(),
     install_requires=[
         "joblib",
--- spektral/__init__.py
+++ spektral/__init__.py
@@ -1,3 +1,3 @@
 from . import datasets, layers, utils
 
-__version__ = "1.0.7"
+__version__ = "1.0.8"
