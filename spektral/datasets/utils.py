241ab5e518c5c9351aa70a89fc1c9bd409f038dc
danielegrattarola <daniele.grattarola@gmail.com>
2022-03-01 17:58:07 +0100

    Format

--- spektral/datasets/utils.py
+++ spektral/datasets/utils.py
@@ -9,6 +9,7 @@ _dataset_folder = os.path.join("~", "spektral", "datasets")
 _config_path = osp.expanduser(os.path.join("~", "spektral", "config.json"))
 if osp.isfile(_config_path):
     import json
+
     with open(_config_path) as fh:
         _config = json.load(fh)
         _dataset_folder = _config.get("dataset_folder", _dataset_folder)
