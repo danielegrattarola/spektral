9ad520732030e796461f5d2a9855a633df70c99a
danielegrattarola <daniele.grattarola@gmail.com>
2022-07-22 16:41:22 +0200

    Minor issue

--- tests/test_layers/convolutional/test_gin_conv_batch.py
+++ tests/test_layers/convolutional/test_gin_conv_batch.py
@@ -3,7 +3,7 @@ from core import MODES, run_layer
 from spektral import layers
 
 config = {
-    "layer": layers.GinConvBatch,
+    "layer": layers.GINConvBatch,
     "modes": [MODES["BATCH"]],
     "kwargs": {"channels": 8, "activation": "relu", "mlp_hidden": [16]},
     "dense": True,
