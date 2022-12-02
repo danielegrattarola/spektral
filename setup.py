afd0f5137650c2081a8c3831b3cd392e875d0fd8
Lucas Miranda <lucas_miranda@psych.mpg.de>
2022-12-02 19:13:27 +0100

    Update setup.py
    Updated setup.py for the package to reflect specific dependencies when running on Apple Silicon (it now installs tensorflow-macos instead of tensorflow).

--- setup.py
+++ setup.py
@@ -1,4 +1,22 @@
 from setuptools import find_packages, setup
+import platform
+
+requirements = [
+    "joblib",
+    "lxml",
+    "networkx",
+    "numpy",
+    "pandas",
+    "requests",
+    "scikit-learn",
+    "scipy",
+    "tqdm",
+]
+
+if platform.processor() != "arm":
+    requirements.append("tensorflow>=2.2.0")
+else:
+    requirements.append("tensorflow-macos>=2.5.0")
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -7,18 +25,7 @@ setup(
     name="spektral",
     version="1.2.0",
     packages=find_packages(),
-    install_requires=[
-        "joblib",
-        "lxml",
-        "networkx",
-        "numpy",
-        "pandas",
-        "requests",
-        "scikit-learn",
-        "scipy",
-        "tensorflow>=2.2.0",
-        "tqdm",
-    ],
+    install_requires=requirements,
     url="https://github.com/danielegrattarola/spektral",
     license="MIT",
     author="Daniele Grattarola",
